43011b2f566f7b27c8c84d81dfd4f6b59c6e2e2c
dulgarianla <dulgarianla17@gmail.com>
2023-09-15 12:23:07 -0600

    all done!

--- AlgorithmLib/BetterLinearSearch.cs
+++ AlgorithmLib/BetterLinearSearch.cs
@@ -4,7 +4,17 @@ public static class BetterLinearSearch
 {
     public static int Search(List<IComparable> data, IComparable target)
     {
-        // ADD CODE HERE AND FIX RETURN STATEMENT
+        // Loop through the elements of the List
+        for (int i = 0; i < data.Count; i++)
+        {
+            // Check if the current element is equal to the target using the Equals method
+            if (data[i].Equals(target))
+            {
+                // If a match is found, return the index i
+                return i;
+            }
+        }
+        // If no match is found in the entire List, return -1
         return -1;
     }
 }
